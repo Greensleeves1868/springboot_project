5ae345c9c09288f030e458c3cfeaa983a3ac185f
Greensleeves1868 <zerofighter0819@gmail.com>
2025-12-28 10:40:41 +0900

    modify

--- job/src/main/java/com/spr/job/JobApplication.java
+++ job/src/main/java/com/spr/job/JobApplication.java
@@ -3,7 +3,7 @@ package com.spr.job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationl
+@SpringBootApplication
 public class JobApplication {
 
 	public static void main(String[] args) {
